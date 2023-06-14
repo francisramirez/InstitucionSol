37ddaee06b3ba65c6660fe28f73793f7c6c9ae24
Francis Ramirez <Francis Ramirez@LAPTOP-DF01N7UN>
2023-06-13 20:06:09 -0400

    Repositorio de curso completado.

--- School.Infrastructure/Interfaces/ICourseRepository.cs
+++ School.Infrastructure/Interfaces/ICourseRepository.cs
@@ -1,6 +1,4 @@
-﻿
-
-using School.Domain.Entities;
+﻿using School.Domain.Entities;
 using School.Domain.Repository;
 using School.Infrastructure.Models;
 using System.Collections.Generic;
@@ -10,5 +8,6 @@ namespace School.Infrastructure.Interfaces
     public interface ICourseRepository : IRepositoryBase<Course>
     {
         List<CursoModel> GetCoursesByDepartment(int departmentId);
+        List<CursoModel> GetCourses();
     }
 }
--- School.Infrastructure/Repositories/CourseRepository.cs
+++ School.Infrastructure/Repositories/CourseRepository.cs
@@ -22,6 +22,65 @@ namespace School.Infrastructure.Repositories
             this.logger = logger;
             this.context = context;
         }
+        
+        public override void Add(Course entity)
+        {
+
+            if (this.Exists(cd => cd.Title == entity.Title))
+                throw new CourseException("El curso ya existe.");
+
+            base.Add(entity);
+            base.SaveChanges();
+        }
+
+        public override void Update(Course entity)
+        {
+            try
+            {
+                Course courseToUpdate = base.GetEntity(entity.CourseID);
+
+                if (courseToUpdate is null)
+                    throw new CourseException("El curso no existe.");
+                
+                courseToUpdate.Credits = entity.Credits;
+                courseToUpdate.DepartmentID = entity.DepartmentID;
+                courseToUpdate.ModifyDate = DateTime.Now;
+                courseToUpdate.Title = entity.Title;
+                courseToUpdate.UserMod = entity.UserMod;
+
+                base.Update(courseToUpdate);
+                base.SaveChanges();
+                
+            }
+            catch (Exception ex) 
+            {
+
+                this.logger.LogError("Ocurrió un error actualizando el curso", ex.ToString());
+            }
+        }
+        public override void Remove(Course entity)
+        {
+            try
+            {
+                Course courseToRemove = base.GetEntity(entity.CourseID);
+
+                if (courseToRemove is null)
+                    throw new CourseException("El curso no existe.");
+
+
+                courseToRemove.Deleted = true;
+                courseToRemove.DeletedDate = DateTime.Now;
+                courseToRemove.UserDeleted = entity.UserDeleted;
+
+                base.Update(courseToRemove);
+                base.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError("Ocurrió un error actualizando el curso", ex.ToString());
+            }
+        }
+
         public List<CursoModel> GetCoursesByDepartment(int departmentId)
         {
             List<CursoModel> cursos = new List<CursoModel>();
@@ -29,11 +88,12 @@ namespace School.Infrastructure.Repositories
             try
             {
 
-                this.logger.LogInformation($"Pase por aqui: { departmentId }");
+                this.logger.LogInformation($"Pase por aqui: {departmentId}");
 
                 cursos = (from cu in base.GetEntities()
                           join de in context.Departments.ToList() on cu.DepartmentID equals de.DepartmentID
-                          where cu.DepartmentID == departmentId
+                          where cu.DepartmentID == departmentId 
+                           && !cu.Deleted
                           select new CursoModel()
                           {
                               CourseId = cu.CourseID,
@@ -54,19 +114,35 @@ namespace School.Infrastructure.Repositories
             return cursos;
         }
 
-        public override void Add(Course entity)
+        public List<CursoModel> GetCourses()
         {
+            List<CursoModel> cursos = new List<CursoModel>();
 
-            if (this.Exists(cd => cd.Title == entity.Title))
-                throw new CourseException("El curso ya existe.");
-            
+            try
+            {
+             
+                cursos = (from cu in base.GetEntities()
+                          join de in context.Departments.ToList() on cu.DepartmentID equals de.DepartmentID
+                          where !cu.Deleted
+                          select new CursoModel()
+                          {
+                              CourseId = cu.CourseID,
+                              Credits = cu.Credits,
+                              DeparmentName = de.Name,
+                              DepartmentID = de.DepartmentID,
+                              Title = cu.Title
+                          }).ToList();
 
 
-            base.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                this.logger.LogError($"Error obeteniendo los cursos: {ex.Message}", ex.ToString());
+            }
+
+            return cursos;
         }
-
-
-
     }
 }
 
